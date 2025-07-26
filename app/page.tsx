620daf1f94cae5d7be733bb9219bda18cdd9c5ea
LeVietHa05 <ha.leviet.0504@gmail.com>
2025-07-26 12:44:00 +0700

    Add Footer component with contact information and integrate into homepage

--- app/page.tsx
+++ app/page.tsx
@@ -5,9 +5,7 @@ import Banner from "./compoment/banner";
 import Donation from "./compoment/donation";
 import Carousel from "./compoment/carousel";
 import Form from "./compoment/form";
-import Link from "next/link";
-
-import { Firlest, Opaline, Vogue } from "./fonts/fonts";
+import Footer from "./compoment/footer";
 
 export default function Home() {
   return (
@@ -26,57 +24,7 @@ export default function Home() {
       <Donation />
 
       {/* FOOTER */}
-      <footer className="p-10 max-w-5xl mx-auto ">
-        <div className={`${Firlest.className} text-center text-3xl mb-4`}>
-          Thông tin liên hệ
-        </div>
-        {/* Nội dung chia 3 cột */}
-        <div className="grid grid-cols-1 md:grid-cols-3 gap-8 text-center">
-          {/* Truyền thông */}
-          <div>
-            <h3 className="font-semibold text-lg">Phan Hải Phong</h3>
-            <p className="text-sm mb-2 text-yellow-300 italic">
-              (Ban truyền thông)
-            </p>
-            <div className="flex items-center justify-center gap-2">
-              <span>📞</span>
-              <span className="font-semibold text-yellow-100">
-                0356 113 090
-              </span>
-            </div>
-          </div>
-
-          {/* Tài chính */}
-          <div>
-            <h3 className="font-semibold text-lg">Nguyễn Vũ Linh Chi</h3>
-            <p className="text-sm mb-2 text-yellow-300 italic">
-              (Ban tài chính)
-            </p>
-            <div className="flex items-center justify-center gap-2">
-              <span>📞</span>
-              <span className="font-semibold text-yellow-100">
-                0337 395 308
-              </span>
-            </div>
-          </div>
-
-          {/* Fanpage */}
-          <div>
-            <h3 className="font-semibold text-lg">Fanpage</h3>
-            <div className="flex items-center justify-center gap-2 mt-2">
-              <span>📘</span>
-              <Link
-                href="https://www.facebook.com/profile.php?id=61578245217922"
-                target="_blank"
-                rel="noopener noreferrer"
-                className="font-semibold text-yellow-100 hover:underline"
-              >
-                Aria Project
-              </Link>
-            </div>
-          </div>
-        </div>
-      </footer>
+      <Footer />
     </div>
   );
 }
